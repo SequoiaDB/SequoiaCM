6b5da4b1c6e0e6bbee03cd78961ff5d6938a6ea2
huangqiaohui <huangqiaohui@sequoiadb.com>
2020-02-17 14:47:31 +0800

    SEQUOIACM-492:listws时，不存在datasharding、metasharding、dataoption时，不打印这些列

--- src/content-server/project/common/tools/src/main/java/com/sequoiacm/tools/printor/ScmWorkspaceInfoPrinter.java
+++ src/content-server/project/common/tools/src/main/java/com/sequoiacm/tools/printor/ScmWorkspaceInfoPrinter.java
@@ -12,9 +12,9 @@ public class ScmWorkspaceInfoPrinter {
     private List<String> idList = new ArrayList<>();
     private List<String> metaList = new ArrayList<>();
     private List<String> dataList = new ArrayList<>();
-    private List<String> sharTypeList = new ArrayList<>();
+    private List<String> dataSharTypeList = new ArrayList<>();
     private List<String> metaShardTypeList = new ArrayList<>();
-    private List<String> lobPageSizeList = new ArrayList<>();
+    private List<String> dataOpsList = new ArrayList<>();
 
     private String colOfName = "Name";
     private String colOfId = "Id";
@@ -26,11 +26,12 @@ public class ScmWorkspaceInfoPrinter {
     private int maxNameLen = colOfName.length() + 1;
     private int maxIdLen = colOfId.length() + 1;
     private int maxMetaShardTypeLen = colOfMetaShardType.length() + 1;
-    private int maxShardTypeLen = colOfShardType.length() + 1;
+    private int maxDataShardTypeLen = colOfShardType.length() + 1;
     private int maxMetaLen = colOfMeta.length() + 1;
-    private int maxLobPageSizeLen = colOfDataOption.length() + 1;
+    private int maxDataOpsLen = colOfDataOption.length() + 1;
 
     public ScmWorkspaceInfoPrinter(List<ScmWorkspaceInfo> list) throws ScmToolsException {
+
         for (ScmWorkspaceInfo info : list) {
             nameList.add(info.getName());
             if (info.getName().length() >= maxNameLen) {
@@ -41,15 +42,15 @@ public class ScmWorkspaceInfoPrinter {
                 maxIdLen = (info.getId() + "").length() + 1;
             }
 
-            String shardType = info.getDataShardingTypeBSON() == null ? "null" : info
-                    .getDataShardingTypeBSON().toString();
-            sharTypeList.add(shardType);
-            if (shardType.length() >= maxShardTypeLen) {
-                maxShardTypeLen = shardType.length() + 1;
+            String shardType = info.getDataShardingTypeBSON() == null ? "null"
+                    : info.getDataShardingTypeBSON().toString();
+            dataSharTypeList.add(shardType);
+            if (shardType.length() >= maxDataShardTypeLen) {
+                maxDataShardTypeLen = shardType.length() + 1;
             }
 
-            String metaShardType = info.getMetaShardType() == null ? "null" : info
-                    .getMetaShardType();
+            String metaShardType = info.getMetaShardType() == null ? "null"
+                    : info.getMetaShardType();
             metaShardTypeList.add(metaShardType);
             if (metaShardType.length() >= maxMetaShardTypeLen) {
                 maxMetaShardTypeLen = metaShardType.length() + 1;
@@ -60,11 +61,11 @@ public class ScmWorkspaceInfoPrinter {
                 maxMetaLen = (info.getMetaLocationBSON().toString()).length() + 1;
             }
 
-            String dataOpStr = info.getDataOptionBSON() == null ? "null" : info.getDataOptionBSON()
-                    .toString();
-            lobPageSizeList.add(dataOpStr);
-            if (dataOpStr.length() >= maxLobPageSizeLen) {
-                maxLobPageSizeLen = dataOpStr.length() + 1;
+            String dataOpStr = info.getDataOptionBSON() == null ? "null"
+                    : info.getDataOptionBSON().toString();
+            dataOpsList.add(dataOpStr);
+            if (dataOpStr.length() >= maxDataOpsLen) {
+                maxDataOpsLen = dataOpStr.length() + 1;
             }
 
             dataList.add(info.getDataLocationBSON().toString());
@@ -79,14 +80,23 @@ public class ScmWorkspaceInfoPrinter {
         System.out.print(colOfId);
         ScmCommon.printSpace(maxIdLen - colOfId.length());
 
-        System.out.print(colOfMetaShardType);
-        ScmCommon.printSpace(maxMetaShardTypeLen - colOfMetaShardType.length());
+        boolean isMetaShardTypeListHasValue = isContainNotNull(metaShardTypeList);
+        if (isMetaShardTypeListHasValue) {
+            System.out.print(colOfMetaShardType);
+            ScmCommon.printSpace(maxMetaShardTypeLen - colOfMetaShardType.length());
+        }
 
-        System.out.print(colOfShardType);
-        ScmCommon.printSpace(maxShardTypeLen - colOfShardType.length());
+        boolean isDataShardTypeListHasValue = isContainNotNull(dataSharTypeList);
+        if (isDataShardTypeListHasValue) {
+            System.out.print(colOfShardType);
+            ScmCommon.printSpace(maxDataShardTypeLen - colOfShardType.length());
+        }
 
-        System.out.print(colOfDataOption);
-        ScmCommon.printSpace(maxLobPageSizeLen - colOfDataOption.length());
+        boolean isDataOpsListHasValue = isContainNotNull(dataOpsList);
+        if (isDataOpsListHasValue) {
+            System.out.print(colOfDataOption);
+            ScmCommon.printSpace(maxDataOpsLen - colOfDataOption.length());
+        }
 
         System.out.print(colOfMeta);
         ScmCommon.printSpace(maxMetaLen - colOfMeta.length());
@@ -99,14 +109,20 @@ public class ScmWorkspaceInfoPrinter {
             System.out.print(idList.get(i));
             ScmCommon.printSpace(maxIdLen - idList.get(i).length());
 
-            System.out.print(metaShardTypeList.get(i));
-            ScmCommon.printSpace(maxMetaShardTypeLen - metaShardTypeList.get(i).length());
+            if (isMetaShardTypeListHasValue) {
+                System.out.print(metaShardTypeList.get(i));
+                ScmCommon.printSpace(maxMetaShardTypeLen - metaShardTypeList.get(i).length());
+            }
 
-            System.out.print(sharTypeList.get(i));
-            ScmCommon.printSpace(maxShardTypeLen - sharTypeList.get(i).length());
+            if (isDataShardTypeListHasValue) {
+                System.out.print(dataSharTypeList.get(i));
+                ScmCommon.printSpace(maxDataShardTypeLen - dataSharTypeList.get(i).length());
+            }
 
-            System.out.print(lobPageSizeList.get(i));
-            ScmCommon.printSpace(maxLobPageSizeLen - lobPageSizeList.get(i).length());
+            if (isDataOpsListHasValue) {
+                System.out.print(dataOpsList.get(i));
+                ScmCommon.printSpace(maxDataOpsLen - dataOpsList.get(i).length());
+            }
 
             System.out.print(metaList.get(i));
             ScmCommon.printSpace(maxMetaLen - metaList.get(i).length());
@@ -115,5 +131,18 @@ public class ScmWorkspaceInfoPrinter {
         }
 
         System.out.println("Total:" + nameList.size());
+    }
+
+    private boolean isContainNotNull(List<String> list) {
+        if (list == null || list.size() == 0) {
+            return false;
+        }
+
+        for (String s : list) {
+            if (s != null && !s.equals("null")) {
+                return true;
+            }
+        }
+        return false;
     }
 }
