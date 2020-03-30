de2217e457c4873a21e6e356b3df26b51f5d1eb7
fanyu <fanyu@sequoiadb.com>
2020-03-30 20:07:06 +0800

    ci#877 使用驱动检查权限可能会导致部分节点有权限，部分节点没有权限，因此改为使用rest接口检查每个节点的权限

--- testcases/v2.0/story/java/src/test/java/com/sequoiacm/testcommon/scmutils/ScmAuthUtils.java
+++ testcases/v2.0/story/java/src/test/java/com/sequoiacm/testcommon/scmutils/ScmAuthUtils.java
@@ -1,23 +1,32 @@
+
 package com.sequoiacm.testcommon.scmutils;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.UUID;
 
 import org.apache.log4j.Logger;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.web.client.HttpClientErrorException;
+import org.springframework.web.client.RestTemplate;
 import org.testng.Assert;
 
 import com.sequoiacm.client.core.ScmDirectory;
 import com.sequoiacm.client.core.ScmFactory;
-import com.sequoiacm.client.core.ScmFile;
 import com.sequoiacm.client.core.ScmRole;
 import com.sequoiacm.client.core.ScmSession;
 import com.sequoiacm.client.core.ScmUser;
 import com.sequoiacm.client.core.ScmWorkspace;
-import com.sequoiacm.client.element.ScmId;
 import com.sequoiacm.client.element.privilege.ScmPrivilegeType;
 import com.sequoiacm.client.element.privilege.ScmResource;
 import com.sequoiacm.client.element.privilege.ScmResourceFactory;
 import com.sequoiacm.client.exception.ScmException;
 import com.sequoiacm.exception.ScmError;
+import com.sequoiacm.testcommon.NodeWrapper;
 import com.sequoiacm.testcommon.SiteWrapper;
 import com.sequoiacm.testcommon.TestScmBase;
 import com.sequoiacm.testcommon.TestScmTools;
@@ -26,7 +35,18 @@ import com.sequoiacm.testcommon.WsWrapper;
 public class ScmAuthUtils extends TestScmBase {
     private static final Logger logger = Logger.getLogger( ScmAuthUtils.class );
     private static final int defaultTimeOut = 1 * 30 * 1000; // 0.5min
-    private static final int sleepTime = 1 * 1000;  // 10s
+    private static final int sleepTime = 200;  // 200ms
+    private static RestTemplate rest;
+
+    static {
+        HttpComponentsClientHttpRequestFactory factory = new
+                HttpComponentsClientHttpRequestFactory();
+        factory.setConnectionRequestTimeout( 10000 );
+        factory.setConnectTimeout( 10000 );
+        factory.setBufferRequestBody( false );
+        factory.setReadTimeout( 30000 );
+        rest = new RestTemplate( factory );
+    }
 
     /**
      * check privilege is effect by wsp
@@ -44,8 +64,10 @@ public class ScmAuthUtils extends TestScmBase {
      * @author huangxiaoni
      */
     public static void checkPriority( SiteWrapper site, String username,
-            String password, ScmRole role, String wsName ) throws Exception {
+            String password, ScmRole role, String wsName )
+            throws Exception {
         ScmSession ss = null;
+        ScmSession newSS = null;
         try {
             // login
             ss = TestScmTools.createSession( site );
@@ -54,42 +76,36 @@ public class ScmAuthUtils extends TestScmBase {
             ScmUser user = ScmFactory.User.getUser( ss, username );
             Assert.assertTrue( user.hasRole( role ) );
 
-            //create path
+            //create scm dir
+            List< NodeWrapper > nodeList = site.getNodes();
+            List< ScmDirectory > scmDirs = new ArrayList<>();
             String dirPath =
                     "/ScmAuthUtils" + "_" + username + "_" + UUID.randomUUID();
-            ScmDirectory dir = ScmFactory.Directory
-                    .createInstance( ws, dirPath );
-            String fileName =
-                    "ScmAuthUtils" + "_" + username + "_" + UUID.randomUUID();
-
-            // grant privilege
+            for ( int i = 0; i < nodeList.size(); i++ ) {
+                ScmDirectory dir = ScmFactory.Directory
+                        .createInstance( ws, dirPath );
+                if ( i == 0 ) {
+                    ScmResource resource = ScmResourceFactory
+                            .createDirectoryResource( wsName, dirPath );
+                    // grant privilege
+                    ScmFactory.Role.grantPrivilege( ss, role, resource,
+                            ScmPrivilegeType.DELETE );
+                }
+                scmDirs.add( dir );
+                dirPath = dirPath + "/" + i;
+            }
             int version1 = ScmFactory.Privilege.getMeta( ss ).getVersion();
-            ScmResource resource = ScmResourceFactory
-                    .createDirectoryResource( wsName, dirPath );
-            ScmFactory.Role.grantPrivilege( ss, role, resource,
-                    ScmPrivilegeType.DELETE );
-
-            //create file for check privilege
-            ScmFile file = ScmFactory.File.createInstance( ws );
-            file.setFileName( fileName );
-            file.setDirectory( dir );
-            ScmId fileId = file.save();
-
-            // check privilege is effect
-            ScmSession newSS = null;
-            try {
-                newSS = TestScmTools.createSession( site, username, password );
-                ScmWorkspace newWS = ScmFactory.Workspace
-                        .getWorkspace( wsName, newSS );
+            newSS = TestScmTools.createSession( site, username, password );
+            for ( int i = scmDirs.size() - 1; i >= 0; i-- ) {
                 int maxTimes = defaultTimeOut / sleepTime;
                 while ( maxTimes-- > 0 ) {
                     try {
-                        Thread.sleep( sleepTime );
                         //the newWS used to check privilege come into effect
-                        ScmFactory.File.deleteInstance( newWS, fileId, true );
-                        ScmFactory.Directory.deleteInstance( ws, dirPath );
+                        deleteScmDirByRest( newSS, nodeList.get( i ),
+                                wsName, scmDirs.get( i ) );
                         return;
                     } catch ( ScmException e ) {
+                        Thread.sleep( sleepTime );
                         if ( ScmError.OPERATION_UNAUTHORIZED == e.getError() ) {
                             logger.warn( username + " has tried " + (
                                     defaultTimeOut / sleepTime - maxTimes )
@@ -100,10 +116,11 @@ public class ScmAuthUtils extends TestScmBase {
                             logger.error(
                                     "failed to wait privilege come into " +
                                             "effect,version1 = "
+
                                             + version1 + ",version2 = "
                                             + ScmFactory.Privilege.getMeta( ss )
-                                            .getVersion() + ",fileId = "
-                                            + fileId );
+                                            .getVersion() + ",scmDir = "
+                                            + scmDirs.get( i ).getId() );
                             throw e;
                         }
                     }
@@ -113,15 +130,36 @@ public class ScmAuthUtils extends TestScmBase {
                                 " = "
                                 + version1 + ",version2 = "
                                 + ScmFactory.Privilege.getMeta( ss )
-                                .getVersion() + ",fileId = " + fileId );
-            } finally {
-                if ( null != newSS ) {
-                    newSS.close();
-                }
+                                .getVersion() + ",scmDirid = " + scmDirs.get(
+                                i ).getId() );
             }
         } finally {
-            if ( null != ss ) {
+            if ( ss != null ) {
                 ss.close();
+            }
+            if ( newSS != null ) {
+                newSS.close();
+            }
+        }
+    }
+
+    private static void deleteScmDirByRest( ScmSession session,
+            NodeWrapper node,
+            String wsName, ScmDirectory scmDirectory ) throws ScmException {
+        HttpHeaders requestHeaders = new HttpHeaders();
+        requestHeaders.add( "x-auth-token", session.getSessionId() );
+        HttpEntity entity = new HttpEntity<>( new LinkedMultiValueMap<>(),
+                requestHeaders );
+        try {
+            rest.exchange( "http://" + node.getUrl() +
+                            "/api/v1/directories/id/"
+                            + scmDirectory.getId() + "?workspace_name=" +
+                            wsName, HttpMethod.DELETE, entity,
+                    String.class );
+        } catch ( HttpClientErrorException e ) {
+            if ( e.getResponseBodyAsString() != null && e
+                    .getResponseBodyAsString().contains( "-109" ) ) {
+                throw new ScmException( -109, e.getResponseBodyAsString() );
             }
         }
     }
