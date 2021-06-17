334a134dd1973b277eab706642fb10d46bb559e9
yipan <yipan@sequoiadb.com>
2021-06-17 16:59:40 +0800

    SEQUOIACM-665提交自动化用例

---
+++ testcases/v2.0/story/java/src/test/java/com/sequoiacm/auth/S3AuthServer3624.java
@@ -0,0 +1,43 @@
+package com.sequoiacm.auth;
+
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.model.AmazonS3Exception;
+import com.sequoiacm.testcommon.*;
+import com.sequoiacm.testcommon.scmutils.S3Utils;
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+/**
+ * @Descreption SCM-3624:用户不存在，进行登录
+ * @Author YiPan
+ * @Date 2021/6/17
+ */
+public class S3AuthServer3624 extends TestScmBase {
+    private AmazonS3 amazonS3 = null;
+    private String accessKey = "test";
+    private String secretKey = "test";
+    private String bucketName = "bucket_3624";
+
+    @BeforeClass(alwaysRun = true)
+    private void setUp() {
+    }
+
+    @Test(groups = { "oneSite", "twoSite", "fourSite" })
+    private void test() {
+        amazonS3 = S3Utils.buildS3Client( accessKey, secretKey );
+        try {
+            amazonS3.doesBucketExistV2( bucketName );
+            Assert.fail( "exp failed but act success!!!" );
+        } catch ( AmazonS3Exception e ) {
+            Assert.assertEquals( e.getStatusCode(), 403 );
+            Assert.assertEquals( e.getErrorCode(), "InvalidAccessKeyId" );
+        }
+    }
+
+    @AfterClass(alwaysRun = true)
+    private void tearDown() {
+        amazonS3.shutdown();
+    }
+}
