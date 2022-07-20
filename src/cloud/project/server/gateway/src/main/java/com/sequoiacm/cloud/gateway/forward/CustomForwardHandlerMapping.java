b305bfd1f2223bbaa5169f04486c44b4dc80835c
huangqiaohui <huangqiaohui@sequoiadb.com>
2022-07-20 10:49:37 +0800

    SEQUOIACM-853：解决网关自定义转发捕获到tomcat内置/error请求的问题

--- src/cloud/project/server/gateway/src/main/java/com/sequoiacm/cloud/gateway/forward/CustomForwardHandlerMapping.java
+++ src/cloud/project/server/gateway/src/main/java/com/sequoiacm/cloud/gateway/forward/CustomForwardHandlerMapping.java
@@ -24,8 +24,8 @@ import com.sequoiacm.infrastructure.monitor.ReqRecorder;
 public class CustomForwardHandlerMapping extends AbstractHandlerMapping {
     private static final String SCM_CUSTOM_FORWARD_DECISION_ATTRIBUTE = CustomForwardHandlerMapping.class
             .getName() + ".DECISION";
-    private static final String SCM_MAPPING_FAILED_MARKER_ATTRIBUTE = CustomForwardHandlerMapping.class
-            .getName() + ".MAPPING.FAILED.MARKER";
+    private static final String SCM_CUSTOM_MAPPING_MARKER_ATTRIBUTE = CustomForwardHandlerMapping.class
+            .getName() + ".MAPPING.MARKER";
     private static final Logger logger = LoggerFactory.getLogger(CustomForwardHandlerMapping.class);
     private final List<CustomForwardDecider> deciders;
     private final HandlerMethod handlerMethod;
@@ -49,37 +49,36 @@ public class CustomForwardHandlerMapping extends AbstractHandlerMapping {
 
     @Override
     protected Object getHandlerInternal(HttpServletRequest request) {
-        // 当本函数抛出异常时，设置这个属性
-        if (request.getAttribute(SCM_MAPPING_FAILED_MARKER_ATTRIBUTE) != null) {
-            // 获取 handler 抛出异常时，spring 会修改请求的 url 为 /error 重新遍历 handler mapping 获取 handler
+        // 该请求被本 Mapping 处理过时，设置这个属性
+        if (request.getAttribute(SCM_CUSTOM_MAPPING_MARKER_ATTRIBUTE) != null) {
+            // 当请求处理失败时，tomcat 会修改请求的 url 为 /error 令 spring 重新遍历 handler mapping 获取 error
+            // handler
             // 这个 handler mapping 不能处理 /error，返回 null 让 spring 找下一个 handler mapping
             return null;
         }
-        try {
-            Decision decision = null;
-            for (CustomForwardDecider decider : deciders) {
-                decision = decider.decide(request);
-                if (decision.isCustomForward()) {
-                    break;
-                }
-            }
-            if (decision == null || !decision.isCustomForward()) {
-                return null;
-            }
+        request.setAttribute(SCM_CUSTOM_MAPPING_MARKER_ATTRIBUTE,
+                SCM_CUSTOM_MAPPING_MARKER_ATTRIBUTE);
 
-            request.setAttribute(SCM_CUSTOM_FORWARD_DECISION_ATTRIBUTE, decision);
-            request.setAttribute(BEST_MATCHING_PATTERN_ATTRIBUTE,
-                    "/" + decision.getServiceName() + "/**");
-            return handlerMethod;
+        Decision decision = null;
+        for (CustomForwardDecider decider : deciders) {
+            decision = decider.decide(request);
+            if (decision.isCustomForward()) {
+                break;
+            }
         }
-        catch (Exception e) {
-            request.setAttribute(SCM_MAPPING_FAILED_MARKER_ATTRIBUTE, SCM_MAPPING_FAILED_MARKER_ATTRIBUTE);
-            throw e;
+        if (decision == null || !decision.isCustomForward()) {
+            return null;
         }
+
+        request.setAttribute(SCM_CUSTOM_FORWARD_DECISION_ATTRIBUTE, decision);
+        request.setAttribute(BEST_MATCHING_PATTERN_ATTRIBUTE,
+                "/" + decision.getServiceName() + "/**");
+        return handlerMethod;
+
     }
 
     public void forward(HttpServletRequest clientReq, HttpServletResponse clientResp)
-            throws IOException {
+            throws Exception {
         Decision decision = (Decision) clientReq.getAttribute(SCM_CUSTOM_FORWARD_DECISION_ATTRIBUTE);
         long before = System.currentTimeMillis();
         try {
@@ -92,8 +91,8 @@ public class CustomForwardHandlerMapping extends AbstractHandlerMapping {
         catch (Exception e) {
             logger.error("failed to forward request: serviceName={}, reqUrl={}, queryParam={}",
                     decision.getServiceName(), clientReq.getRequestURI(),
-                    clientReq.getParameterMap(), e);
-            clientResp.sendError(500, e.getMessage());
+                    clientReq.getQueryString());
+            throw e;
         }
         finally {
             ReqRecorder.getInstance().addRecord(System.currentTimeMillis() - before);
