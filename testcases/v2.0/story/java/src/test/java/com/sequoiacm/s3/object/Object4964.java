7f5025435b948eab61da0c91c2bfdd7125929421
zhangyanan <zhangyanan@sequoiadb.com>
2022-07-20 17:01:57 +0800

    s3y预签名url访问对象补充自动化用例

---
+++ testcases/v2.0/story/java/src/test/java/com/sequoiacm/s3/object/Object4964.java
@@ -0,0 +1,286 @@
+package com.sequoiacm.s3.object;
+
+import com.amazonaws.services.s3.model.BucketVersioningConfiguration;
+import com.amazonaws.services.s3.model.GeneratePresignedUrlRequest;
+import com.amazonaws.services.s3.model.SetBucketVersioningConfigurationRequest;
+import com.amazonaws.util.Md5Utils;
+import com.sequoiacm.testcommon.*;
+import org.springframework.core.io.Resource;
+import org.springframework.http.RequestEntity;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.client.HttpClientErrorException;
+import org.springframework.web.client.RestTemplate;
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import com.amazonaws.services.s3.AmazonS3;
+import com.sequoiacm.testcommon.scmutils.S3Utils;
+import java.io.File;
+import java.net.URI;
+import java.net.URL;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.concurrent.atomic.AtomicInteger;
+
+/**
+ * @descreption SCM-4964:创建预签名生成url访问对象
+ * @author ZhangYanan
+ * @Date 2022/7/20
+ * @updateUser
+ * @updateDate
+ * @updateRemark
+ * @version 1.0
+ */
+public class Object4964 extends TestScmBase {
+    private int expSuccessNum = 6;
+    private AtomicInteger total = new AtomicInteger( 0 );
+    private String bucketName1 = "bucket4964a";
+    private String bucketName2 = "bucket4964b";
+    private String objectName1 = "object4964a";
+    private String objectName2 = "object4964b";
+    private String content = "4964";
+    private double versionNum = 4.0;
+    private AmazonS3 s3Client = null;
+    private int fileSize = 1024 * 200;
+    private File localPath = null;
+    private String filePath = null;
+    private RestTemplate restTemplate = new RestTemplate();
+
+    @BeforeClass
+    private void setUp() throws Exception {
+        localPath = new File( TestScmBase.dataDirectory + File.separator
+                + TestTools.getClassName() );
+        TestTools.LocalFile.removeFile( localPath );
+        TestTools.LocalFile.createDir( localPath.toString() );
+        filePath = localPath + File.separator + "localFile_" + fileSize
+                + ".txt";
+        TestTools.LocalFile.createFile( filePath, fileSize );
+        s3Client = S3Utils.buildS3Client();
+        S3Utils.clearBucket( s3Client, bucketName1 );
+        S3Utils.clearBucket( s3Client, bucketName2 );
+
+        s3Client.createBucket( bucketName1 );
+        s3Client.createBucket( bucketName2 );
+
+        BucketVersioningConfiguration config = new BucketVersioningConfiguration()
+                .withStatus( BucketVersioningConfiguration.ENABLED );
+        s3Client.setBucketVersioningConfiguration(
+                new SetBucketVersioningConfigurationRequest( bucketName2,
+                        config ) );
+
+        // 禁用版本控制桶下对象
+        s3Client.putObject( bucketName1, objectName1, new File( filePath ) );
+        s3Client.putObject( bucketName1, objectName2, content );
+
+        // 开启版本控制桶下对象
+        s3Client.putObject( bucketName2, objectName1, "" );
+        s3Client.putObject( bucketName2, objectName1, content );
+        s3Client.putObject( bucketName2, objectName1, new File( filePath ) );
+    }
+
+    // 有效期内获取对象、对象内容为文件、禁用版本控制
+    @Test
+    public void test1() throws Exception {
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTimeInMillis( new Date().getTime() + 1000 * 60 * 60 );
+        // 有效时长为1小时
+        Date expireTime = calendar.getTime();
+        GeneratePresignedUrlRequest request = new GeneratePresignedUrlRequest(
+                bucketName1, objectName1 ).withExpiration( expireTime )
+                        .withMethod( com.amazonaws.HttpMethod.GET );
+        // 生成预签名
+        URL url = s3Client.generatePresignedUrl( request );
+        // 通过预签名获取对象
+        RequestEntity< ? > requestEntity = new RequestEntity<>(
+                org.springframework.http.HttpMethod.GET,
+                new URI( url.toString() ) );
+        ResponseEntity< Resource > resp = restTemplate.exchange( requestEntity,
+                Resource.class );
+        // 检查结果
+        Assert.assertEquals(
+                Md5Utils.md5AsBase64( resp.getBody().getInputStream() ),
+                Md5Utils.md5AsBase64( new File( filePath ) ),
+                "url = " + url.toString() );
+        total.getAndIncrement();
+    }
+
+    // 有效期内获取对象、对象内容为字符串、禁用版本控制
+    @Test
+    public void test2() throws Exception {
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTimeInMillis( new Date().getTime() + 1000 * 60 * 24 );
+        // 有效时长为24小时
+        Date expireTime = calendar.getTime();
+        GeneratePresignedUrlRequest request = new GeneratePresignedUrlRequest(
+                bucketName1, objectName2 ).withExpiration( expireTime )
+                        .withMethod( com.amazonaws.HttpMethod.GET );
+        // 生成预签名
+        URL url = s3Client.generatePresignedUrl( request );
+        // 通过预签名获取对象
+        RequestEntity< ? > requestEntity = new RequestEntity<>(
+                org.springframework.http.HttpMethod.GET,
+                new URI( url.toString() ) );
+        ResponseEntity< String > resp = restTemplate.exchange( requestEntity,
+                String.class );
+        // 检查结果
+        Assert.assertEquals( resp.getBody(), content );
+        total.getAndIncrement();
+    }
+
+    // 开启版本控制、文件大小为0、200K等
+    @Test
+    public void test3() throws Exception {
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTimeInMillis( new Date().getTime() + 1000 * 60 * 24 );
+        // 有效时长为24小时
+        Date expireTime = calendar.getTime();
+        for ( double i = 1.0; i < versionNum; i++ ) {
+            GeneratePresignedUrlRequest request = new GeneratePresignedUrlRequest(
+                    bucketName2, objectName1 ).withVersionId( "" + i )
+                            .withExpiration( expireTime )
+                            .withMethod( com.amazonaws.HttpMethod.GET );
+            // 生成预签名
+            URL url = s3Client.generatePresignedUrl( request );
+            // 通过预签名获取对象
+            RequestEntity< ? > requestEntity = new RequestEntity<>(
+                    org.springframework.http.HttpMethod.GET,
+                    new URI( url.toString() ) );
+            ResponseEntity< Resource > resp = restTemplate
+                    .exchange( requestEntity, Resource.class );
+            // 检查结果
+            if ( i == 1.0 ) {
+                Assert.assertNull(resp.getBody(), "url = " + url.toString());
+            } else if ( i == 2.0 ) {
+                Assert.assertEquals(
+                        Md5Utils.md5AsBase64( resp.getBody().getInputStream() ),
+                        Md5Utils.md5AsBase64( content.getBytes() ),
+                        "url = " + url.toString() );
+            } else {
+                Assert.assertEquals(
+                        Md5Utils.md5AsBase64( resp.getBody().getInputStream() ),
+                        Md5Utils.md5AsBase64( new File( filePath ) ),
+                        "url = " + url.toString() );
+            }
+        }
+        total.getAndIncrement();
+    }
+
+    // 过期后获取对象
+    @Test
+    public void test4() throws Exception {
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTimeInMillis( new Date().getTime() + 1000 );
+        // 有效时长为1秒
+        Date expireTime = calendar.getTime();
+        // 生成预签名
+        URL url = s3Client.generatePresignedUrl( bucketName1, objectName1,
+                expireTime );
+        // 休眠5秒，使其过期
+        int interval = 1000;
+        int count = 60;
+        boolean flag = false;
+        // 通过预签名获取对象
+        while ( count-- > 0 ) {
+            RequestEntity< ? > requestEntity = new RequestEntity<>(
+                    org.springframework.http.HttpMethod.GET,
+                    new URI( url.toString() ) );
+            try {
+                restTemplate.exchange( requestEntity, Resource.class );
+                Thread.sleep( interval );
+            } catch ( HttpClientErrorException e ) {
+                flag = true;
+                if ( e.getStatusCode().value() != 403 ) {
+                    throw new Exception( e.getResponseBodyAsString() );
+                }
+            }
+            if ( flag ) {
+                break;
+            }
+        }
+        if ( !flag ) {
+            throw new Exception( "time out!!! url = " + url.toString() );
+        }
+        total.getAndIncrement();
+    }
+
+    // bucketName或者keyName不存在
+    @Test
+    public void test5() throws Exception {
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTimeInMillis( new Date().getTime() + 1000 );
+        // 有效时长为1秒
+        Date expireTime = calendar.getTime();
+        // 桶不存在，生成预签名
+        URL url1 = s3Client.generatePresignedUrl( bucketName1 + "A",
+                objectName1, expireTime );
+        RequestEntity< ? > requestEntity1 = new RequestEntity<>(
+                org.springframework.http.HttpMethod.GET,
+                new URI( url1.toString() ) );
+        try {
+            restTemplate.exchange( requestEntity1, Resource.class );
+            Assert.fail( "exp failed but act success!!!" );
+        } catch ( HttpClientErrorException e ) {
+            if ( e.getStatusCode().value() != 404
+                    && e.getStatusCode().value() != 403 ) {
+                throw new Exception( e.getResponseBodyAsString() );
+            }
+        }
+
+        // 对象不存在，生成预签名
+        URL url2 = s3Client.generatePresignedUrl( bucketName1,
+                objectName1 + "A", expireTime );
+        RequestEntity< ? > requestEntity2 = new RequestEntity<>(
+                org.springframework.http.HttpMethod.GET,
+                new URI( url2.toString() ) );
+        try {
+            restTemplate.exchange( requestEntity2, Resource.class );
+            Assert.fail( "exp failed but act success!!!" );
+        } catch ( HttpClientErrorException e ) {
+            if ( e.getStatusCode().value() != 404
+                    && e.getStatusCode().value() != 403 ) {
+                throw new Exception( e.getResponseBodyAsString() );
+            }
+        }
+        total.getAndIncrement();
+    }
+
+    // expireTime小于当前时间
+    @Test
+    public void test6() throws Exception {
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTimeInMillis( new Date().getTime() - 24 * 60 * 60 * 1000 );
+        // 有效时长为1秒
+        Date expireTime = calendar.getTime();
+        URL url1 = s3Client.generatePresignedUrl( bucketName1, objectName1,
+                expireTime );
+        RequestEntity< ? > requestEntity1 = new RequestEntity<>(
+                org.springframework.http.HttpMethod.GET,
+                new URI( url1.toString() ) );
+        try {
+            restTemplate.exchange( requestEntity1, Resource.class );
+            Assert.fail( "exp failed but act success!!!" );
+        } catch ( HttpClientErrorException e ) {
+            if ( e.getStatusCode().value() != 403 ) {
+                throw new Exception( e.getResponseBodyAsString() );
+            }
+        }
+        total.getAndIncrement();
+    }
+
+    @AfterClass
+    private void tearDown() {
+        try {
+            if ( total.get() == expSuccessNum ) {
+                S3Utils.clearBucket( s3Client, bucketName1 );
+                S3Utils.clearBucket( s3Client, bucketName2 );
+                TestTools.LocalFile.removeFile( localPath );
+            }
+        } finally {
+            if ( s3Client != null ) {
+                s3Client.shutdown();
+            }
+        }
+    }
+}
---
+++ testcases/v2.0/story/java/src/test/java/com/sequoiacm/s3/object/Object4965.java
@@ -0,0 +1,122 @@
+package com.sequoiacm.s3.object;
+
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.util.Calendar;
+import java.util.Date;
+
+import com.amazonaws.ClientConfiguration;
+import com.amazonaws.auth.AWSCredentials;
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
+import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.client.builder.AwsClientBuilder;
+import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+import com.amazonaws.services.s3.model.GeneratePresignedUrlRequest;
+import org.springframework.core.io.Resource;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.RequestEntity;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.client.RestTemplate;
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import com.amazonaws.services.s3.AmazonS3;
+import com.sequoiacm.testcommon.TestScmBase;
+import com.sequoiacm.testcommon.scmutils.S3Utils;
+
+/**
+ * @descreption SCM-4965:通过预签名获取url，使用head object方式访问
+ * @author ZhangYanan
+ * @Date 2022/7/20
+ * @updateUser
+ * @updateDate
+ * @updateRemark
+ * @version 1.0
+ */
+public class Object4965 extends TestScmBase {
+    private static String clientRegion = "us-east-1";
+    private String bucketName = "bucket24256";
+    private String keyName = "testkey_24256";
+    private String file = "object24256";
+    private AmazonS3 s3ClientV4 = null;
+    private AmazonS3 s3ClientV2 = null;
+    private boolean runSuccess = false;
+
+    @BeforeClass
+    private void setUp() throws Exception {
+        s3ClientV4 = S3Utils.buildS3Client();
+        s3ClientV2 = buildS3ClientV2();
+        S3Utils.clearBucket( s3ClientV4, bucketName );
+
+        s3ClientV4.createBucket( bucketName );
+        s3ClientV4.putObject( bucketName, keyName, file );
+    }
+
+    @Test
+    public void test() throws Exception {
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTimeInMillis( new Date().getTime() + 1000 * 10 );
+        Date expireTime = calendar.getTime();
+
+        GeneratePresignedUrlRequest request = new GeneratePresignedUrlRequest(
+                bucketName, keyName ).withExpiration( expireTime )
+                        .withMethod( com.amazonaws.HttpMethod.HEAD );
+
+        URL urlv4 = s3ClientV4.generatePresignedUrl( request );
+        URL urlv2 = s3ClientV2.generatePresignedUrl( request );
+        checkResponse( urlv4 );
+        checkResponse( urlv2 );
+        runSuccess = true;
+    }
+
+    @AfterClass
+    private void tearDown() {
+        try {
+            if ( runSuccess ) {
+                S3Utils.clearBucket( s3ClientV4, bucketName );
+            }
+        } finally {
+            if ( s3ClientV2 != null ) {
+                s3ClientV2.shutdown();
+            }
+            if ( s3ClientV4 != null ) {
+                s3ClientV4.shutdown();
+            }
+        }
+    }
+
+    private AmazonS3 buildS3ClientV2() {
+        AmazonS3 s3Client;
+        AWSCredentials credentials = new BasicAWSCredentials(
+                TestScmBase.s3AccessKeyID, TestScmBase.s3SecretKey );
+        AwsClientBuilder.EndpointConfiguration endpointConfiguration = new AwsClientBuilder.EndpointConfiguration(
+                S3Utils.getS3Url(), clientRegion );
+        ClientConfiguration config = new ClientConfiguration();
+        config.setUseExpectContinue( false );
+        config.setSocketTimeout( 300000 );
+        // 以v2方式生成连接
+        config.setSignerOverride( "S3SignerType" );
+        s3Client = AmazonS3ClientBuilder.standard()
+                .withEndpointConfiguration( endpointConfiguration )
+                .withClientConfiguration( config )
+                .withChunkedEncodingDisabled( true )
+                .withPathStyleAccessEnabled( true )
+                .withCredentials(
+                        new AWSStaticCredentialsProvider( credentials ) )
+                .build();
+        return s3Client;
+    }
+
+    private void checkResponse( URL url ) throws URISyntaxException {
+        RestTemplate restTemplate = new RestTemplate();
+        RequestEntity< ? > requestEntity = new RequestEntity<>( HttpMethod.HEAD,
+                new URI( url.toString() ) );
+        ResponseEntity< Resource > resp = restTemplate.exchange( requestEntity,
+                Resource.class );
+        Assert.assertEquals( resp.getStatusCode(), HttpStatus.OK );
+    }
+}
