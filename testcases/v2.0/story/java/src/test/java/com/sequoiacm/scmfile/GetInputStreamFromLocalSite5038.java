d5c91e0ff756584b0c186104305a0b12603c97c7
yipan <yipan@sequoiadb.com>
2022-08-01 17:26:11 +0800

    SEQUOIACM-859提交自动化用例

---
+++ testcases/v2.0/story/java/src/test/java/com/sequoiacm/scmfile/GetInputStreamFromLocalSite5038.java
@@ -0,0 +1,107 @@
+package com.sequoiacm.scmfile;
+
+import com.sequoiacm.client.core.*;
+import com.sequoiacm.client.element.ScmId;
+import com.sequoiacm.client.exception.ScmException;
+import com.sequoiacm.common.CommonDefine;
+import com.sequoiacm.exception.ScmError;
+import com.sequoiacm.testcommon.*;
+import com.sequoiacm.testcommon.scmutils.S3Utils;
+import com.sequoiacm.testcommon.scmutils.ScmFileUtils;
+import org.bson.BSONObject;
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import java.io.File;
+import java.io.IOException;
+
+/**
+ * @descreption SCM-5038:getInputStreamFromLocalSite接口测试
+ * @author YiPan
+ * @date 2022/7/25
+ * @updateUser
+ * @updateDate
+ * @updateRemark
+ * @version 1.0
+ */
+public class GetInputStreamFromLocalSite5038 extends TestScmBase {
+    private String fileName = "file5038";
+    private File localPath = null;
+    private String filePath = null;
+    private String downloadPath = null;
+    private int fileSize = 1024 * 10;
+    private ScmSession rootSiteSession;
+    private ScmSession branchSiteSession;
+    private ScmWorkspace rootSiteWs;
+    private ScmWorkspace branchSiteWs;
+    private ScmId fileId;
+    private boolean runSuccess = false;
+
+    @BeforeClass
+    private void setUp() throws IOException, ScmException {
+        localPath = new File( TestScmBase.dataDirectory + File.separator
+                + TestTools.getClassName() );
+        filePath = localPath + File.separator + "localFile_" + fileSize
+                + ".txt";
+        downloadPath = localPath + File.separator + "downLoadFile_" + fileSize
+                + ".txt";
+        TestTools.LocalFile.removeFile( localPath );
+        TestTools.LocalFile.createDir( localPath.toString() );
+        TestTools.LocalFile.createFile( filePath, fileSize );
+
+        WsWrapper wsp = ScmInfo.getWs();
+        rootSiteSession = TestScmTools.createSession( ScmInfo.getRootSite() );
+        branchSiteSession = TestScmTools
+                .createSession( ScmInfo.getBranchSite() );
+        rootSiteWs = ScmFactory.Workspace.getWorkspace( wsp.getName(),
+                rootSiteSession );
+        branchSiteWs = ScmFactory.Workspace.getWorkspace( wsp.getName(),
+                branchSiteSession );
+        BSONObject query = ScmQueryBuilder
+                .start( ScmAttributeName.File.FILE_NAME ).is( fileName ).get();
+        ScmFileUtils.cleanFile( wsp, query );
+
+        ScmFile file = ScmFactory.File.createInstance( rootSiteWs );
+        file.setFileName( fileName );
+        file.setContent( filePath );
+        fileId = file.save();
+    }
+
+    @Test(groups = { "twoSite", "fourSite" })
+    private void test() throws Exception {
+        // 主站点下载
+        ScmFile rootSiteFile = ScmFactory.File.getInstance( rootSiteWs,
+                fileId );
+        S3Utils.inputStream2File( rootSiteFile.getInputStreamFromLocalSite(),
+                downloadPath );
+        Assert.assertEquals( TestTools.getMD5( filePath ),
+                TestTools.getMD5( downloadPath ) );
+
+        // 分站点，不缓存下载
+        ScmFile branchSiteFile = ScmFactory.File.getInstance( branchSiteWs,
+                fileId );
+        try {
+            branchSiteFile.getInputStreamFromLocalSite();
+        } catch ( ScmException e ) {
+            if ( e.getError() != ScmError.DATA_NOT_EXIST ) {
+                throw e;
+            }
+        }
+        runSuccess = true;
+    }
+
+    @AfterClass
+    private void tearDown() throws ScmException {
+        if ( runSuccess ) {
+            try {
+                ScmFactory.File.deleteInstance( rootSiteWs, fileId, true );
+                TestTools.LocalFile.removeFile( localPath );
+            } finally {
+                rootSiteSession.close();
+                branchSiteSession.close();
+            }
+        }
+    }
+}
