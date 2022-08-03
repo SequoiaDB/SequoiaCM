61ec0210a8c535a0b251441a29bf3b466a6b3aae
yipan <yipan@sequoiadb.com>
2022-08-03 11:10:41 +0800

    SEQUOIACM-1014暂时屏蔽用例

--- testcases/v2.0/story/java/src/test/java/com/sequoiacm/s3/object/concurrent/Object4242.java
+++ testcases/v2.0/story/java/src/test/java/com/sequoiacm/s3/object/concurrent/Object4242.java
@@ -50,7 +50,8 @@ public class Object4242 extends TestScmBase {
         s3Client.putObject( bucketName, key, text1 );
     }
 
-    @Test
+    // TODO: SEQUOIACM-1014
+    @Test(enabled = false)
     public void test() throws Exception {
         ThreadExecutor te = new ThreadExecutor();
         S3UpdateObject t1 = new S3UpdateObject();
