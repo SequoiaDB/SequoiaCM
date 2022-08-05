6913608ee4a48d712df7a8f99a26866ab766e9ea
zhangyanan <zhangyanan@sequoiadb.com>
2022-08-05 14:02:34 +0800

    修改rest接口读超时时间

--- testcases/v2.0/story/java/src/test/java/com/sequoiacm/testcommon/RestWrapper.java
+++ testcases/v2.0/story/java/src/test/java/com/sequoiacm/testcommon/RestWrapper.java
@@ -26,7 +26,7 @@ public class RestWrapper {
         factory.setConnectionRequestTimeout( 10000 );
         factory.setConnectTimeout( 10000 );
         factory.setBufferRequestBody( false );
-        factory.setReadTimeout( 30000 );
+        factory.setReadTimeout( 60000 );
         rest = new RestTemplate( factory );
     }
 
