24100e016bde557a11e3104931c2c51e9d6e7aba
zhaoyujing <16-zhaoyujing@users.noreply.gitlab.example.com>
2022-09-30 09:21:13 +0800

    SEQUOIACM-1080 S3 服务的 V2 版本预签名识别存在问题

--- src/s3/project/server/s3-server/src/main/java/com/sequoiacm/s3/authoriztion/S3AuthorizationFactory.java
+++ src/s3/project/server/s3-server/src/main/java/com/sequoiacm/s3/authoriztion/S3AuthorizationFactory.java
@@ -4,6 +4,7 @@ import java.util.regex.Pattern;
 
 import javax.servlet.http.HttpServletRequest;
 
+import com.sequoiacm.s3.common.RestParamDefine;
 import com.sequoiacm.s3.config.AuthorizationConfig;
 import com.sequoiacm.s3.exception.S3Error;
 import com.sequoiacm.s3.exception.S3ServerException;
@@ -16,7 +17,8 @@ public class S3AuthorizationFactory {
     @Autowired
     private AuthorizationConfig authConfig;
 
-    public S3Authorization createAuthentication(HttpServletRequest webRequest) throws S3ServerException {
+    public S3Authorization createAuthentication(HttpServletRequest webRequest)
+            throws S3ServerException {
         try {
             AuthorizationType type = getAuthorizationType(webRequest);
             if (null == type) {
@@ -58,23 +60,44 @@ public class S3AuthorizationFactory {
 }
 
 enum AuthorizationType {
-    V2("^AWS .*:.*"),
-    V4("^AWS4-HMAC-SHA256.*"),
-    PRE_SIGN_V2(".*&Signature=.*"),
-    PRE_SIGN_V4(".*&X-Amz-Signature=.*");
+    V2 {
+        @Override
+        public boolean match(HttpServletRequest webRequest) {
+            return isMatchAuthorizationHeader("^AWS .*:.*", webRequest);
+        }
+    },
+    V4 {
+        @Override
+        public boolean match(HttpServletRequest webRequest) {
+            return isMatchAuthorizationHeader("^AWS4-HMAC-SHA256.*", webRequest);
+        }
+    },
+    PRE_SIGN_V2 {
+        @Override
+        public boolean match(HttpServletRequest webRequest) {
+            return isMatchParameter(RestParamDefine.SignatureV2.SIGNATURE, webRequest);
+        }
+    },
+    PRE_SIGN_V4 {
+        @Override
+        public boolean match(HttpServletRequest webRequest) {
+            return isMatchParameter(RestParamDefine.SignatureV4.X_AMZ_ALGORITHM, webRequest);
+        }
+    };
 
-    private String regex;
+    public abstract boolean match(HttpServletRequest webRequest);
 
-    AuthorizationType(String authenticationHeaderRgex) {
-        this.regex = authenticationHeaderRgex;
+    private static boolean isMatchAuthorizationHeader(String regex, HttpServletRequest webRequest) {
+        String authString = webRequest.getHeader(S3Authorization.AUTHORIZATION_HEADER);
+        if (null != authString && Pattern.matches(regex, authString)) {
+            return true;
+        }
+
+        return false;
     }
 
-    public boolean match(HttpServletRequest webRequest) {
-        String authString = webRequest.getHeader(S3Authorization.AUTHORIZATION_HEADER);
-        if (null == authString) {
-            authString = webRequest.getQueryString();
-        }
-        if (authString != null && Pattern.matches(regex, authString)) {
+    private static boolean isMatchParameter(String parameterName, HttpServletRequest webRequest) {
+        if (webRequest.getParameter(parameterName) != null) {
             return true;
         }
 
