6dd3e8e2fd1462cb086c8f070e35da8f37ca5bda
zhangyanan <zhangyanan@sequoiadb.com>
2022-10-12 13:55:21 +0800

    问题单995补充自动化用例

---
+++ testcases/v2.0/story/java/src/test/java/com/sequoiacm/scmfile/ScmFile5348.java
@@ -0,0 +1,101 @@
+package com.sequoiacm.scmfile;
+
+import java.io.File;
+import java.io.IOException;
+
+import org.bson.BSONObject;
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import com.sequoiacm.client.core.*;
+import com.sequoiacm.client.exception.ScmException;
+import com.sequoiacm.exception.ScmError;
+import com.sequoiacm.testcommon.*;
+import com.sequoiacm.testcommon.listener.GroupTags;
+import com.sequoiacm.testcommon.scmutils.ScmFileUtils;
+
+/**
+ * @descreption SCM-5348:按文件路径删除已被删除的多版本文件
+ * @author ZhangYanan
+ * @date 2022/10/12
+ * @updateUser
+ * @updateDate
+ * @updateRemark
+ * @version 1.0
+ */
+public class ScmFile5348 extends TestScmBase {
+    private boolean runSuccess = false;
+    private SiteWrapper site = null;
+    private WsWrapper wsp = null;
+    private ScmSession session = null;
+    private ScmWorkspace ws = null;
+    private String fileName = "file5348";
+    private int fileSize = 1024 * 1024;
+    private String filePath1 = null;
+    private String filePath2 = null;
+    private File localPath = null;
+    private BSONObject queryCond = null;
+
+    @BeforeClass()
+    private void setUp() throws ScmException, IOException {
+        localPath = new File( TestScmBase.dataDirectory + File.separator
+                + TestTools.getClassName() );
+        filePath1 = localPath + File.separator + "localFile1_" + fileSize
+                + ".txt";
+        filePath2 = localPath + File.separator + "localFile2_" + fileSize
+                + ".txt";
+
+        TestTools.LocalFile.removeFile( localPath );
+        TestTools.LocalFile.createDir( localPath.toString() );
+        TestTools.LocalFile.createFile( filePath1, fileSize );
+        TestTools.LocalFile.createFile( filePath2, fileSize );
+
+        site = ScmInfo.getSite();
+
+        wsp = ScmInfo.getWs();
+        session = TestScmTools.createSession( site );
+        ws = ScmFactory.Workspace.getWorkspace( wsp.getName(), session );
+
+        queryCond = ScmQueryBuilder.start( ScmAttributeName.File.AUTHOR )
+                .is( fileName ).get();
+        ScmFileUtils.cleanFile( wsp, queryCond );
+    }
+
+    @Test(groups = { GroupTags.oneSite, GroupTags.twoSite, GroupTags.fourSite })
+    public void test() throws Exception {
+        // 文件存在单个站点
+        ScmFile file = ScmFactory.File.createInstance( ws );
+        file.setContent( filePath1 );
+        file.setFileName( fileName );
+        file.setAuthor( fileName );
+        file.save();
+        file.updateContent( filePath2 );
+
+        ScmFactory.File.deleteInstanceByPath( ws, "/" + fileName, true );
+        try {
+            ScmFactory.File.deleteInstanceByPath( ws, "/" + fileName, true );
+            Assert.fail( "the file expected not exist" );
+        } catch ( ScmException e ) {
+            if ( e.getErrorCode() != ScmError.FILE_NOT_FOUND.getErrorCode() ) {
+                throw e;
+            }
+        }
+
+        runSuccess = true;
+    }
+
+    @AfterClass()
+    private void tearDown() {
+        try {
+            if ( runSuccess || TestScmBase.forceClear ) {
+                TestTools.LocalFile.removeFile( localPath );
+            }
+        } finally {
+            if ( session != null ) {
+                session.close();
+            }
+        }
+    }
+}
