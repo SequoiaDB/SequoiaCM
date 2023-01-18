b94c12f4e7a438dbd39fd5fad4265a307281d3af
yangjianbo <149-yangjianbo@users.noreply.gitlab.example.com>
2023-01-18 16:32:18 +0800

    SEQUOIACM-1223:scm部署后密码文件路径带连续的两个/

--- tools/scm-deploy/src/main/java/com/sequoiacm/deploy/module/BasicInstallConfig.java
+++ tools/scm-deploy/src/main/java/com/sequoiacm/deploy/module/BasicInstallConfig.java
@@ -19,13 +19,38 @@ public class BasicInstallConfig {
 
     public BasicInstallConfig(BSONObject bson) {
         installPath = BsonUtils.getStringChecked(bson, ConfFileDefine.INSTALLCONFIG_PATH);
-        if (!installPath.endsWith("/sequoiacm")) {
-            installPath += "/sequoiacm";
-        }
+        installPath = spliceScmInstallPath(installPath);
         installUser = BsonUtils.getString(bson, ConfFileDefine.INSTALLCONFIG_USER);
         if (installUser == null || installUser.length() <= 0) {
             installUser = "scmadmin";
         }
+    }
+
+    /*
+     * splice scm installPath no repeat linux file separator
+     * for example:
+     * ""                    => /sequoiacm
+     * /opt                 => /opt/sequoiacm
+     * /opt/                => /opt/sequoiacm
+     * /opt////             => /opt/sequoiacm
+     * /opt//sequoiacm//    => /opt/sequoiacm
+     */
+    public String spliceScmInstallPath(String path) {
+        path = path.replaceAll("/{2,}", "/");
+        if (!path.endsWith("/sequoiacm")) {
+            if (path.endsWith("/sequoiacm/")) {
+                path = path.substring(0, path.length() - 1);
+            }
+            else {
+                if (path.endsWith("/")) {
+                    path += "sequoiacm";
+                }
+                else {
+                    path += "/sequoiacm";
+                }
+            }
+        }
+        return path;
     }
 
     public String getInstallPath() {
