eaf1ab67706b3a11f48ae7d78132c314479fce30
zhangyanan <zhangyanan@sequoiadb.com>
2023-05-06 18:30:46 +0800

    CI-1942:调整直连hdfs数据源公共方法

--- testcases/v2.0/story/java/src/test/java/com/sequoiacm/testcommon/dsutils/HdfsUtils.java
+++ testcases/v2.0/story/java/src/test/java/com/sequoiacm/testcommon/dsutils/HdfsUtils.java
@@ -37,7 +37,15 @@ public class HdfsUtils extends TestScmBase {
      */
     public static FileSystem getFs( SiteWrapper site ) {
         Configuration conf = new Configuration();
-        conf.set( urlKey, TestScmBase.hdfsURI );
+        conf.set( urlKey, "hdfs://ns1" );
+        conf.set( "dfs.nameservices", "ns1" );
+        conf.set( "dfs.ha.namenodes.ns1", "nn1,nn2" );
+        conf.set( "dfs.client.failover.proxy.provider.ns1",
+                "org.apache.hadoop.hdfs.server.namenode.ha.ConfiguredFailoverProxyProvider" );
+        conf.set( "dfs.namenode.rpc-address.ns1.nn2",
+                "scm-test-ci-cent7:9000" );
+        conf.set( "dfs.namenode.rpc-address.ns1.nn1",
+                "scm-test-ci-cent8:9000" );
         conf.set( "fs.hdfs.impl",
                 "org.apache.hadoop.hdfs.DistributedFileSystem" );
         FileSystem fs = null;
