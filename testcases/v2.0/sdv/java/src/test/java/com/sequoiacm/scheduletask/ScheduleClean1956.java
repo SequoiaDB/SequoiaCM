d025235288eaf963bbd6a6efc2f9e880f59b7e9f
zhangyanan <zhangyanan@sequoiadb.com>
2023-05-25 17:33:33 +0800

    CI-2025:屏蔽因标签检索功能合入失败用例

--- testcases/v2.0/sdv/java/src/test/java/com/sequoiacm/scheduletask/ScheduleClean1956.java
+++ testcases/v2.0/sdv/java/src/test/java/com/sequoiacm/scheduletask/ScheduleClean1956.java
@@ -44,8 +44,8 @@ import com.sequoiacm.testcommon.scmutils.ScmScheduleUtils;
 import javax.swing.*;
 
 /**
- * @Description:SCM-1956 :: 创建异步调度任务，设置类型为清理，条件为自定义属性和标签
  * @author fanyu
+ * @Description:SCM-1956 :: 创建异步调度任务，设置类型为清理，条件为自定义属性和标签
  * @Date:2018年7月11日
  * @version:1.0
  */
@@ -62,10 +62,10 @@ public class ScheduleClean1956 extends TestScmBase {
     private String name = "ScheduleClean1956";
     private int fileNum = 50;
     private int fileSize = 1024 * 1;
-    private List< ScmId > fileIdList = new ArrayList< ScmId >();
+    private List<ScmId> fileIdList = new ArrayList<ScmId>();
 
-    private List< String > tagList = new ArrayList< String >();
-    private List< String > propertiesList = new ArrayList< String >();
+    private List<String> tagList = new ArrayList<String>();
+    private List<String> propertiesList = new ArrayList<String>();
     private ScmClass class1 = null;
     private ScmAttribute attr = null;
 
@@ -79,179 +79,181 @@ public class ScheduleClean1956 extends TestScmBase {
 
     @BeforeClass
     private void setUp() throws Exception {
-        localPath = new File( TestScmBase.dataDirectory + File.separator
-                + TestTools.getClassName() );
+        localPath = new File(TestScmBase.dataDirectory + File.separator
+                + TestTools.getClassName());
         filePath = localPath + File.separator + "localFile_" + fileSize
                 + ".txt";
-        TestTools.LocalFile.removeFile( localPath );
-        TestTools.LocalFile.createDir( localPath.toString() );
-        TestTools.LocalFile.createFile( filePath, fileSize );
-        List< SiteWrapper > siteList = ScmInfo
-                .getBranchSites( ScmInfo.getAllSites().size() - 1 );
-        site1 = siteList.get( 0 );
-        site2 = siteList.get( 1 );
+        TestTools.LocalFile.removeFile(localPath);
+        TestTools.LocalFile.createDir(localPath.toString());
+        TestTools.LocalFile.createFile(filePath, fileSize);
+        List<SiteWrapper> siteList = ScmInfo
+                .getBranchSites(ScmInfo.getAllSites().size() - 1);
+        site1 = siteList.get(0);
+        site2 = siteList.get(1);
         wsp = ScmInfo.getWs();
-        session = TestScmTools.createSession( site1 );
-        sessionM = TestScmTools.createSession( site2 );
-        ws = ScmFactory.Workspace.getWorkspace( wsp.getName(), session );
-        wsM = ScmFactory.Workspace.getWorkspace( wsp.getName(), sessionM );
-        BSONObject cond = ScmQueryBuilder.start( ScmAttributeName.File.AUTHOR )
-                .is( name ).get();
-        ScmFileUtils.cleanFile( wsp, cond );
-        calendar.set( Calendar.HOUR, calendar.get( Calendar.HOUR ) - 3 );
-        createAllFile( fileNum );
-        readFile( fileIdList );
+        session = TestScmTools.createSession(site1);
+        sessionM = TestScmTools.createSession(site2);
+        ws = ScmFactory.Workspace.getWorkspace(wsp.getName(), session);
+        wsM = ScmFactory.Workspace.getWorkspace(wsp.getName(), sessionM);
+        BSONObject cond = ScmQueryBuilder.start(ScmAttributeName.File.AUTHOR)
+                .is(name).get();
+        ScmFileUtils.cleanFile(wsp, cond);
+        calendar.set(Calendar.HOUR, calendar.get(Calendar.HOUR) - 3);
+        createAllFile(fileNum);
+        readFile(fileIdList);
     }
 
-    @Test(groups = { "fourSite", GroupTags.star })
+    // 问题单http://jira.web:8080/browse/SEQUOIACM-1312未修改，屏蔽用例
+    @Test(groups = {"fourSite", GroupTags.star}, enabled = false)
     private void testStar() throws Exception {
         BSONObject queryCond = ScmQueryBuilder
-                .start( ScmAttributeName.File.PROPERTIES + "." + name )
-                .lessThan( fileNum / 2 ).and( ScmAttributeName.File.TAGS )
-                .in( tagList.subList( 0, fileNum / 2 ) ).get();
-        createScheduleTask( queryCond );
-        SiteWrapper[] expSites = { ScmInfo.getRootSite(), site1, site2 };
-        ScmScheduleUtils.checkScmFile( ws,
-                fileIdList.subList( fileNum / 2, fileNum ), expSites );
+                .start(ScmAttributeName.File.PROPERTIES + "." + name)
+                .lessThan(fileNum / 2).and(ScmAttributeName.File.TAGS)
+                .in(tagList.subList(0, fileNum / 2)).get();
+        createScheduleTask(queryCond);
+        SiteWrapper[] expSites = {ScmInfo.getRootSite(), site1, site2};
+        ScmScheduleUtils.checkScmFile(ws,
+                fileIdList.subList(fileNum / 2, fileNum), expSites);
 
-        SiteWrapper[] expCleanSites = { ScmInfo.getRootSite(), site2 };
-        ScmScheduleUtils.checkScmFile( ws, fileIdList.subList( 0, fileNum / 2 ),
-                expCleanSites );
+        SiteWrapper[] expCleanSites = {ScmInfo.getRootSite(), site2};
+        ScmScheduleUtils.checkScmFile(ws, fileIdList.subList(0, fileNum / 2),
+                expCleanSites);
         runSuccess = true;
     }
 
-    @Test(groups = { "fourSite", GroupTags.net })
+    // 问题单http://jira.web:8080/browse/SEQUOIACM-1312未修改，屏蔽用例
+    @Test(groups = {"fourSite", GroupTags.net}, enabled = false)
     private void testNet() throws Exception {
         BSONObject queryCond = ScmQueryBuilder
-                .start( ScmAttributeName.File.PROPERTIES + "." + name )
-                .lessThan( fileNum / 2 ).and( ScmAttributeName.File.TAGS )
-                .in( tagList.subList( 0, fileNum / 2 ) ).get();
-        createScheduleTask( queryCond );
-        SiteWrapper[] expSites = { site1, site2 };
-        ScmScheduleUtils.checkScmFile( ws,
-                fileIdList.subList( fileNum / 2, fileNum ), expSites );
+                .start(ScmAttributeName.File.PROPERTIES + "." + name)
+                .lessThan(fileNum / 2).and(ScmAttributeName.File.TAGS)
+                .in(tagList.subList(0, fileNum / 2)).get();
+        createScheduleTask(queryCond);
+        SiteWrapper[] expSites = {site1, site2};
+        ScmScheduleUtils.checkScmFile(ws,
+                fileIdList.subList(fileNum / 2, fileNum), expSites);
 
-        SiteWrapper[] expCleanSites = { site2 };
-        ScmScheduleUtils.checkScmFile( ws, fileIdList.subList( 0, fileNum / 2 ),
-                expCleanSites );
+        SiteWrapper[] expCleanSites = {site2};
+        ScmScheduleUtils.checkScmFile(ws, fileIdList.subList(0, fileNum / 2),
+                expCleanSites);
         runSuccess = true;
     }
 
     @AfterClass
     private void tearDown() throws Exception {
         try {
-            if ( !runSuccess || TestScmBase.forceClear ) {
-                if ( fileIdList != null ) {
-                    for ( ScmId fileId : fileIdList ) {
-                        System.out.println( "fileId = " + fileId.get() );
+            if (!runSuccess || TestScmBase.forceClear) {
+                if (fileIdList != null) {
+                    for (ScmId fileId : fileIdList) {
+                        System.out.println("fileId = " + fileId.get());
                     }
                 }
-                if ( scheduleId != null ) {
-                    System.out.println( "scheduleId = " + scheduleId.get() );
+                if (scheduleId != null) {
+                    System.out.println("scheduleId = " + scheduleId.get());
                 }
             }
-            for ( ScmId fileId : fileIdList ) {
-                ScmFactory.File.deleteInstance( ws, fileId, true );
+            for (ScmId fileId : fileIdList) {
+                ScmFactory.File.deleteInstance(ws, fileId, true);
             }
-            ScmFactory.Class.deleteInstance( ws, class1.getId() );
-            ScmFactory.Attribute.deleteInstance( ws, attr.getId() );
-            TestTools.LocalFile.removeFile( localPath );
-            if ( scheduleId != null ) {
-                ScmSystem.Schedule.delete( session, scheduleId );
+            ScmFactory.Class.deleteInstance(ws, class1.getId());
+            ScmFactory.Attribute.deleteInstance(ws, attr.getId());
+            TestTools.LocalFile.removeFile(localPath);
+            if (scheduleId != null) {
+                ScmSystem.Schedule.delete(session, scheduleId);
             }
-            ScmScheduleUtils.cleanTask( session, scheduleId );
-            TestTools.LocalFile.removeFile( localPath );
+            ScmScheduleUtils.cleanTask(session, scheduleId);
+            TestTools.LocalFile.removeFile(localPath);
         } finally {
-            if ( session != null ) {
+            if (session != null) {
                 session.close();
             }
-            if ( sessionM != null ) {
+            if (sessionM != null) {
                 sessionM.close();
             }
         }
     }
 
-    private void createScheduleTask( BSONObject queryCond )
+    private void createScheduleTask(BSONObject queryCond)
             throws ScmException {
         String maxStayTime = "0d";
         // create schedule task
-        content = new ScmScheduleCleanFileContent( site1.getSiteName(),
-                maxStayTime, queryCond );
+        content = new ScmScheduleCleanFileContent(site1.getSiteName(),
+                maxStayTime, queryCond);
         cron = "* * * * * ?";
-        ScmSchedule sche = ScmSystem.Schedule.create( session, wsp.getName(),
+        ScmSchedule sche = ScmSystem.Schedule.create(session, wsp.getName(),
                 ScheduleType.CLEAN_FILE, name, "ScheduleClean1956", content,
-                cron );
+                cron);
         scheduleId = sche.getId();
     }
 
-    private void readFile( List< ScmId > fileIdList ) throws Exception {
-        for ( ScmId fileId : fileIdList ) {
+    private void readFile(List<ScmId> fileIdList) throws Exception {
+        for (ScmId fileId : fileIdList) {
             // read scmFile at the branSite
-            ScmFile file2 = ScmFactory.File.getInstance( wsM, fileId );
+            ScmFile file2 = ScmFactory.File.getInstance(wsM, fileId);
             String downloadPath = TestTools.LocalFile.initDownloadPath(
                     localPath, TestTools.getMethodName(),
-                    Thread.currentThread().getId() );
-            file2.getContent( downloadPath );
+                    Thread.currentThread().getId());
+            file2.getContent(downloadPath);
         }
     }
 
-    private void createAllFile( int num ) throws ScmException, ParseException {
-        attr = craeteAttr( name );
-        class1 = ScmFactory.Class.createInstance( ws, name, name + "_desc" );
-        class1.attachAttr( attr.getId() );
-        for ( int i = 0; i < num; i++ ) {
+    private void createAllFile(int num) throws ScmException, ParseException {
+        attr = craeteAttr(name);
+        class1 = ScmFactory.Class.createInstance(ws, name, name + "_desc");
+        class1.attachAttr(attr.getId());
+        for (int i = 0; i < num; i++) {
             ScmTags tags = new ScmTags();
-            tags.addTag( String.valueOf( i ) );
-            tagList.add( String.valueOf( i ) );
-            ScmClassProperties properties = createProperties( class1, i );
-            ScmId fileId = createFile( name, properties, tags );
-            propertiesList.add( properties.toString() );
-            fileIdList.add( fileId );
+            tags.addTag(String.valueOf(i));
+            tagList.add(String.valueOf(i));
+            ScmClassProperties properties = createProperties(class1, i);
+            ScmId fileId = createFile(name, properties, tags);
+            propertiesList.add(properties.toString());
+            fileIdList.add(fileId);
         }
     }
 
-    private ScmId createFile( String name, ScmClassProperties properties,
-            ScmTags tag ) throws ScmException, ParseException {
-        ScmFile file = ScmFactory.File.createInstance( ws );
-        file.setFileName( name + "_" + UUID.randomUUID() );
-        file.setAuthor( name );
-        file.setTags( tag );
-        file.setClassProperties( properties );
-        file.setCreateTime( calendar.getTime() );
-        file.setContent( filePath );
+    private ScmId createFile(String name, ScmClassProperties properties,
+                             ScmTags tag) throws ScmException, ParseException {
+        ScmFile file = ScmFactory.File.createInstance(ws);
+        file.setFileName(name + "_" + UUID.randomUUID());
+        file.setAuthor(name);
+        file.setTags(tag);
+        file.setClassProperties(properties);
+        file.setCreateTime(calendar.getTime());
+        file.setContent(filePath);
         ScmId fileId = file.save();
         return fileId;
     }
 
-    private ScmClassProperties createProperties( ScmClass class1, int value ) {
+    private ScmClassProperties createProperties(ScmClass class1, int value) {
         ScmClassProperties properties = new ScmClassProperties(
-                class1.getId().get() );
-        for ( int i = 0; i < class1.listAttrs().size(); i++ ) {
-            properties.addProperty( class1.listAttrs().get( i ).getName(),
-                    value );
+                class1.getId().get());
+        for (int i = 0; i < class1.listAttrs().size(); i++) {
+            properties.addProperty(class1.listAttrs().get(i).getName(),
+                    value);
         }
         return properties;
     }
 
-    private ScmAttribute craeteAttr( String name ) throws ScmException {
+    private ScmAttribute craeteAttr(String name) throws ScmException {
         ScmAttributeConf conf = new ScmAttributeConf();
-        conf.setName( name );
-        conf.setDescription( name + "_desc" );
-        conf.setDisplayName( name + "_display" );
-        conf.setRequired( false );
-        conf.setType( AttributeType.INTEGER );
-        ScmAttribute attr = ScmFactory.Attribute.createInstance( ws, conf );
+        conf.setName(name);
+        conf.setDescription(name + "_desc");
+        conf.setDisplayName(name + "_display");
+        conf.setRequired(false);
+        conf.setType(AttributeType.INTEGER);
+        ScmAttribute attr = ScmFactory.Attribute.createInstance(ws, conf);
         return attr;
     }
 
-    private void checkResult( List< ScmId > fileIdList, boolean isClean )
+    private void checkResult(List<ScmId> fileIdList, boolean isClean)
             throws Exception {
-        if ( !isClean ) {
-            SiteWrapper[] expSites = { ScmInfo.getRootSite(), site1, site2 };
-            ScmScheduleUtils.checkScmFile( ws, fileIdList, expSites );
+        if (!isClean) {
+            SiteWrapper[] expSites = {ScmInfo.getRootSite(), site1, site2};
+            ScmScheduleUtils.checkScmFile(ws, fileIdList, expSites);
         } else {
-            SiteWrapper[] expSites = { ScmInfo.getRootSite(), site2 };
-            ScmScheduleUtils.checkScmFile( ws, fileIdList, expSites );
+            SiteWrapper[] expSites = {ScmInfo.getRootSite(), site2};
+            ScmScheduleUtils.checkScmFile(ws, fileIdList, expSites);
         }
     }
 }
