cf093201826d93244939a1576268734ed9dd022a
tangxiong <tangxiong@sequoiadb.com>
2023-06-26 13:50:50 +0800

    SEQUOIACM-1373：调整登出过期session的日志级别为debug

--- src/cloud/project/server/gateway/src/main/java/com/sequoiacm/cloud/gateway/PostAccessFilter.java
+++ src/cloud/project/server/gateway/src/main/java/com/sequoiacm/cloud/gateway/PostAccessFilter.java
@@ -12,11 +12,18 @@ import com.netflix.zuul.ZuulFilter;
 import com.netflix.zuul.context.RequestContext;
 import com.netflix.zuul.exception.ZuulException;
 import com.sequoiacm.infrastructure.common.SecurityRestField;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
+import org.springframework.util.AntPathMatcher;
 
 public class PostAccessFilter extends ZuulFilter {
     private static final Logger logger = LoggerFactory.getLogger(PostAccessFilter.class);
 
     private static final String ERROR_RESPONSE_HOST_INFO = "SCM-ERROR-HOST";
+
+    private AntPathMatcher matcher = new AntPathMatcher();
+    @Value("${zuul.routes.logout.path:/logout}")
+    private String logoutPath = "/logout";
 
     @Override
     public boolean shouldFilter() {
@@ -32,7 +39,7 @@ public class PostAccessFilter extends ZuulFilter {
             String sessionId = request.getHeader(SecurityRestField.SESSION_ATTRIBUTE);
 
             String target = "unknown host";
-            logger.error("proxy=" + ctx.get("proxy"));
+            logErrorMsg("proxy=" + ctx.get("proxy"), ctx);
             Object o = ctx.get("ribbonResponse");
             if (o instanceof HttpResponse) {
                 HttpResponse rar = (HttpResponse) o;
@@ -48,12 +55,30 @@ public class PostAccessFilter extends ZuulFilter {
                         .valueOf(ctx.get(ApacheHttpClientConfiguration.HTTP_CLIENT_TARGET_HOST));
             }
 
-            logger.error("send {} request {} from {}:{} to {} with session {} failed(status={})",
+            String msg = String.format(
+                    "send %s request %s from %s:%d to %s with session %s failed(status=%d)",
                     request.getMethod(), request.getRequestURI(), request.getRemoteHost(),
                     request.getRemotePort(), target, sessionId, ctx.getResponseStatusCode());
+            logErrorMsg(msg, ctx);
         }
 
         return null;
+    }
+
+    private void logErrorMsg(String msg, RequestContext ctx) {
+        if (ctx.getResponseStatusCode() == HttpStatus.NOT_FOUND.value()
+                && isLogoutRequest(ctx.getRequest())) {
+            // 登出过期 session 时，降低日志级别
+            logger.debug(msg);
+        }
+        else {
+            logger.error(msg);
+        }
+    }
+
+    private boolean isLogoutRequest(HttpServletRequest request) {
+        String uri = request.getRequestURI();
+        return matcher.match(logoutPath, uri);
     }
 
     @Override
